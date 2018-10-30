e80f3ec451e12bcf5d2d4c1b0dcb1af699512078
alessanderleite <alessander.logistica@gmail.com>
2018-10-30 07:17:49 -0300

    creating Select method

--- app/src/main/java/com/example/alessander/firebasecrud/MainActivity.java
+++ app/src/main/java/com/example/alessander/firebasecrud/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alessander.firebasecrud;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,8 +11,11 @@ import android.widget.ListView;
 
 import com.example.alessander.firebasecrud.modelo.Pessoa;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,13 +41,34 @@ public class MainActivity extends AppCompatActivity {
         listV_dados = (ListView) findViewById(R.id.listV_dados);
 
         inicializarFirebase();
-
+        eventoDatabase();
     }
 
+    private void eventoDatabase() {
+        databaseReference.child("Pessoa").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listPessoa.clear();
+                for (DataSnapshot objSnapshot : dataSnapshot.getChildren()) {
+                    Pessoa p = objSnapshot.getValue(Pessoa.class);
+                    listPessoa.add(p);
+                }
+                arrayAdapterPessoa = new ArrayAdapter<Pessoa>(MainActivity.this,
+                        android.R.layout.simple_list_item_1, listPessoa);
+                listV_dados.setAdapter(arrayAdapterPessoa);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(MainActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
     }
 
